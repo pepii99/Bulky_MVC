d2376c603ee2e4b6f0a8c495b9652cde2210be5a
pepi <tpekeop@gmail.com>
2024-06-30 01:46:34 +0300

    Added Category Model and seeding data for categories

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name="Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Sci-fi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Biography", DisplayOrder = 4 },
+                new Category { Id = 5, Name = "Horror", DisplayOrder = 5 },
+                new Category { Id = 6, Name = "Thriller", DisplayOrder = 6 }
+                ); 
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
