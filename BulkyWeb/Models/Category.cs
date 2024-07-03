3d01d210aba8626c979b620d7d58d62459729bcb
pepi <tpekeop@gmail.com>
2024-07-03 23:50:51 +0300

    Added Server side validation and ability to add a new category.

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -7,8 +7,10 @@ namespace BulkyWeb.Models
         public int Id { get; set; }
 
         [Required]
+        [MaxLength(40)]
         public string? Name { get; set; }
 
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100.")]
         public int DisplayOrder { get; set; }
     }
 }
