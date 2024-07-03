f5cf82319efe50b39c83d02d01c004f2b97c3f82
pepi <tpekeop@gmail.com>
2024-07-04 01:49:08 +0300

    Added update and delete functionality of categories and toastr notifications

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -30,9 +30,74 @@ namespace BulkyWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully.";
                 return RedirectToAction("Index");
             }
             return View(obj);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryObj = _db.Categories.Find(id);
+
+            if (categoryObj == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryObj);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully.";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == 0 || id == null)
+            {
+                return NotFound();
+            }
+
+            Category? categoryObj = _db.Categories.Find(id);
+
+            if (categoryObj == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryObj);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully.";
+            return RedirectToAction("Index");
+
         }
     }
 }
