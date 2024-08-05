4de43e62738e9292a040339f6d73ec78d9d29b16
pepi <tpekeop@gmail.com>
2024-08-06 02:17:02 +0300

    Removed duplicate Identity service creation

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -15,8 +15,6 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
-
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
