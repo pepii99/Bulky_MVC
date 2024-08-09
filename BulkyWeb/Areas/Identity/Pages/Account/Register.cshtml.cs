9b979a21a01efcdf268f4e6cfd5ab85ce521e042
pepi <tpekeop@gmail.com>
2024-08-09 17:15:51 +0300

    Added more properties on registration form

--- BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -105,10 +105,16 @@ namespace BulkyWeb.Areas.Identity.Pages.Account
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
 
-            public string? Role { get; set; }
+            public string Role { get; set; }
 
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+
+            public string StreetAddress { get; set; }
+            public string City { get; set; }
+            public string PostalCode { get; set; }
+            public string PhoneNumber { get; set; }
+
         }
 
 
